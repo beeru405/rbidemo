afff2b3ca89ea8cce0550f9d4f99c8eac9b0dc6a
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-03-02 16:23:32 +0530

    Create PalindromeTest.java

---
+++ src/test/java/com/example/PalindromeTest.java
@@ -0,0 +1,39 @@
+package com.training.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PalindromeTest{
+
+
+	@Test
+    public void testWordInString() {
+        String sentence = "hello world";
+        assertTrue(sentence.contains("hello"));
+    }
+
+    @Test
+    public void testWordNotInString() {
+        String sentence = "hello world";
+        assertFalse(sentence.contains("cat"));
+    }
+
+    @Test
+    public void testWordInEmptyString() {
+        String sentence = "";
+        assertFalse(sentence.contains("hello"));
+    }
+
+    @Test
+    public void testWordInStringCaseInsensitive() {
+        String sentence = "hello world";
+        assertTrue(sentence.toLowerCase().contains("hello"));
+    }
+    
+    @Test
+    public void testWordWithSpecialCharacters() {
+        String sentence = "hello world";
+        asse
